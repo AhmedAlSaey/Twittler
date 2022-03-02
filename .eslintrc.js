edbac24d69886bc43f00430edb8c651b9d7a5bc4
AhmedAlSai <16P6000@eng.asu.edu.eg>
2022-03-02 21:51:50 +0200

    🚀 Initial commit

---
+++ .eslintrc.js
@@ -0,0 +1,20 @@
+module.exports = {
+	extends: ['plugin:prettier/recommended'],
+	parser: '@typescript-eslint/parser',
+
+	parserOptions: {
+		ecmaVersion: 2019,
+		tsconfigRootDir: __dirname,
+		sourceType: 'module',
+		project: './tsconfig.json',
+	},
+	rules: {
+		'prettier/prettier': 'warn',
+	},
+	ignorePatterns: [
+		".eslintrc.js"
+	],
+	env: {
+		node: true,
+	},
+};
